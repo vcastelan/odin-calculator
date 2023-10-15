b88f86563ee166b44c34779c5e6e5e5202f7e07d
Urvino <vino.castelan@gmail.com>
2023-10-15 13:49:18 -0700

    added some pseudocode to my javascript

---
+++ script.js
@@ -0,0 +1,20 @@
+// need to add buttons from 0 - 9 and add child nodes to parent calculator node.
+
+// neeed to add 4 functions for add, minus, multiply and divide.
+
+
+//need to add a click event listener to apply functionality when click on all 4 buttons.
+  //if else statements to check type of buttons.
+      //aka if plus, minus, multiply or divide  
+      //and call correct functionality.
+
+// need to add text display to one of my divs to display current steps.
+  //when i choose a button number. display button 
+  //when i choose an operation button. display operation
+  //if i choose another button or equal sign no matter display current value.
+  
+//need to add a clear button to delete any operations or current value.
+  //clear the whole display.
+
+//need to add a delete button to delete current step 
+  //to display delete clear the current text display.
