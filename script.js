9b6cde164edcb3c33b958d8aa284c3b44f69ef20
Urvino <vino.castelan@gmail.com>
2023-10-17 16:44:56 -0700

    adjusted hmtl classes and id. added seperate class for numbers and operations. also fixed clear to display blank

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//add window on load to set it to clear and display 0 on results display
+// window.addEventListener('load', clearAll);
 //add 4 functions for add, minus, multiply and divide.
 const add = (a, b) => a + b;
 
@@ -24,15 +26,14 @@ const divide = (a, b) => a / b;
 let firstNumber = '';
 let operator = '';
 let secondNumber = '';
-const currentDisplay = document.querySelector('.current-display');
-const output = document.querySelector('.results-display');
-const digits = document.querySelectorAll('button');
-
+const currentDisplay = document.querySelector('#current-display');
+const output = document.querySelector('#results-display');
+const buttons = document.querySelectorAll('button');
 
 //create a function operate
   //i: 2 numbers and an operator to call operating functions
   //O: operating functions results.
-function operate(a, b, operator) {
+function operate(operator, a, b) {
   //use and if statement or switch case to check what type of operator is passsed.
   switch (operator) {
     case '+':
@@ -42,30 +43,50 @@ function operate(a, b, operator) {
     case 'x':
       return multiply(a, b)
     case '/':
+      if(b === 0) {
+        return "Error cannot divide by 0"
+      }
       return divide(a, b)
     default:
-      return null
+      return null;
   }
 }
 
-//create a function that stores number. into a display value to use in next step
-function appendNumber(number) {
-  currentDisplay.textContent = number;
-}
- 
-//need to add buttons from 0 - 9 and add child nodes to parent calculator node.
-const buttons = document.querySelectorAll('.btn');
+//need to add a clear button to delete any operations or current value.
+  //clear the whole display.
+  const clear = document.querySelector('#clear');
+  clear.addEventListener('click', clearAll);
 
+  function clearAll() {
+    currentDisplay.textContent = '';
+    output.textContent = '';
+    firstNumber = '';
+    operator = '';
+    secondNumber = '';
+  }
+
+//create a function that stores number. into a display value to use in next step
+//need to add buttons from 0 - 9 and add child nodes to parent calculator node.
 buttons.forEach(button => {
   button.addEventListener('click', function() {
-    if(button.textContent !== 'x') {
-      output.textContent = button.textContent;
-      currentDisplay.textContent += button.textContent;
-    } else if(button.textContent === 'CLEAR') {
+    // o: add button content if not an equal. or add or minus or divide or multiply sign
+    if(button.id !== "clear") {
+      if(!currentDisplay.textContent.includes('=')) {
+        currentDisplay.textContent += button.textContent;
+        // else if it equals any of those operators we want update our current display, and remove our output display.
+      }
+    } else {
+      currentDisplay.textContent = '';
+      output.textContent = '';
       clearAll();
     }
   })
 })
+
+// if(output.textContent.includes('+')) {
+//   output.textContent = operate(a, b, '+');
+// }
+
 
 
 //need to add a click event listener to apply functionality when click on all 4 buttons.
@@ -77,21 +98,6 @@ buttons.forEach(button => {
   //when i choose a button number. display button 
   //when i choose an operation button. display operation
   //if i choose another button or equal sign no matter display current value.
-  
-//need to add a clear button to delete any operations or current value.
-  //clear the whole display.
-function clearAll() {
-  output.textContent = '';
-  firstNumber = '';
-  operator = '';
-  secondNumber = '';
-  currentDisplay.textContent = '';
-}
-
-const clear = document.querySelector('.current-Display');
-clear.addEventListener('click', function() {
-    clearAll();
-});
 
 //need to add a delete button to delete current step 
   //to display delete clear the current text display.
