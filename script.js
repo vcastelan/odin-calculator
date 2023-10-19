c0c57a2e3f2eccfb78adb11ec3db9de2aad48c6d
Urvino <vino.castelan@gmail.com>
2023-10-18 22:45:18 -0700

    adjusted to clear display when solved, as well as adjusted for decimals

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const equal = document.querySelector('.eval');
 
 //need to add a clear button to delete any operations or current value
 const clear = document.querySelector('#clear');
+const deleted = document.querySelector('#delete')
 
 //Create 3 variables for a number, an operator, and another number.
 let firstNumber = '';
@@ -19,11 +20,11 @@ let secondNumber = '';
 //add 4 functions for add, minus, multiply and divide.
 const add = (a, b) => a + b;
 
-const subtract = (a, b) => a - b;
+const subtract = (a, b) => (a - b).toFixed(2);
 
 const multiply = (a ,b) => a * b;
 
-const divide = (a, b) => a / b;
+const divide = (a, b) => (a / b).toFixed(8);
 
 //create a function operate
   //i: 2 numbers and an operator to call operating functions
@@ -51,8 +52,8 @@ function doMath(operator, a, b) {
 digits.forEach(digit => {
   digit.addEventListener('click', function() {
     if(!secondNumber) {
-    inputNumber(digit.textContent);
-    currentDisplay.textContent = firstNumber;
+      inputNumber(digit.textContent);
+      currentDisplay.textContent = firstNumber;
     } else {
       inputNumber(digit.textContent);
       currentDisplay.textContent = secondNumber + ' ' + symbol + ' ' + firstNumber;
@@ -63,9 +64,17 @@ digits.forEach(digit => {
 // event listener for when adding operations. will add a symbol to our display
 operations.forEach(mathSymbol => {
   mathSymbol.addEventListener('click', function() {
-    handleOperator(mathSymbol.textContent);
-    currentDisplay.textContent = secondNumber + ' ' + symbol;
-    // output.textContent = firstNumber;
+    if(!output.textContent) {
+      handleOperator(mathSymbol.textContent);
+      currentDisplay.textContent = secondNumber + ' ' + symbol;
+    } else {
+      currentDisplay.textContent = output.textContent + ' ' + mathSymbol.textContent;
+      secondNumber = output.textContent;
+      output.textContent = '';
+      firstNumber = '';
+      symbol = '';
+      handleOperator(mathSymbol.textContent);
+    }
   });
 });
 
@@ -78,10 +87,20 @@ function inputNumber(num) {
 
 // function to add an operator to our display. needs to add the value to our global variable, change our firstNum and remove the value of our firstNum
 function handleOperator(op) {
-  symbol = op;
-  secondNumber = firstNumber;
-  firstNumber = '';
+  if(firstNumber) {
+    symbol = op;
+    secondNumber = firstNumber;
+    firstNumber = '';
+  } else {
+    symbol = op;
+    firstNumber = '';
+  }
 }
+
+//decimal event to add decimal point to the number
+decimal.addEventListener('click', function(e) {
+  inputNumber(e.target.textContent);
+})
 
 //clear the whole display.
 clear.addEventListener('click', function() {
@@ -91,6 +110,15 @@ clear.addEventListener('click', function() {
   operator = '';
   secondNumber = '';
 });
+
+// delete event to delete last input
+deleted.addEventListener('click', function() {
+  output.textContent = '';
+  currentDisplay.textContent = currentDisplay.textContent.slice(0, -1);
+  // firstNumber = firstNumber.slice(0, -1);
+  // secondNumber = secondNumber.slice(0, -1);
+  // symbol = symbol.slice(0, -1);
+})
 
 // event listener to make the calculator evaluate after pressing equal button.
 equal.addEventListener('click', function() {
